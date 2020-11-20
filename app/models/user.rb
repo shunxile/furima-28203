1e4aef9e83d19b6e7dd9af1ab21b0af1c6c80aee
shunxile <69545945+shunxile@users.noreply.github.com>
2020-11-21 01:12:22 +0900

    ユーザー管理機能　修正①

--- app/models/user.rb
+++ app/models/user.rb
@@ -11,6 +11,7 @@ class User < ApplicationRecord
     validates :first_name, format: { with: /\A[ぁ-んァ-ン一-龥]/, message: "は全角で入力してください。"}
     validates :last_name_kana, format: { with: /\A[ァ-ヶー－]+\z/, message: "は全角カタカナで入力して下さい。"}
     validates :first_name_kana, format: { with: /\A[ァ-ヶー－]+\z/, message: "は全角カタカナで入力して下さい。"}
-    validates :password, format: { with: /\A[a-zA-Z0-9]+\z/, message: "は半角英数で6文字以上入力してください。"}
+    validates :password, format: { with: /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i, message: "は半角英数字混合で6文字以上入力してください。"}
+    validates :birth_date
   end
 end
