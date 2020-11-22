886698746eacc6c09e213b2d100b67ef47a84235
shunxile <69545945+shunxile@users.noreply.github.com>
2020-11-22 16:29:37 +0900

    ActiveStorage・ActiveHashの導入

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,11 +1,27 @@
 class ItemsController < ApplicationController
   before_action :authenticate_user!, except: [:index]
 
+  def index
+    @item = Item.order("created_at DESC")
+  end
+  
   def new
     @item = Item.new
   end
 
   def create
+    @item = Item.new(item_params)
+    if @item.save
+      redirect_to "items/new"
+    else
+      render :new
+    end
 
+    private
+
+    def item_params
+      params.require(:item).permit(:image, :name, :content, :value, :state_id, :shipping_charge_id, :shio_area_id, :ship_day_id, :category_id).merge(user_id: current_user.id)
+      
+    end
   end
 end
--- app/models/item.rb
+++ app/models/item.rb
@@ -1,15 +1,22 @@
 class Item < ApplicationRecord
   has_one :purchase_history
   belongs_to :user
+  has_one_attached :image
+  #extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to :category_id
+  belongs_to :state_id
+  belongs_to :shipping_charge_id
+  belongs_to :ship_area_id
+  belongs_to :ship_day_id
 
   with_options presence: true do
     validates :name
     validates :content
     validates :value
-    validates :state_id
-    validates :shipping_charge_id
-    validates :ship_area_id
-    validates :ship_day_id
-    validates :category_id
+    validates :state_id, numericality: { other_than: 1 }
+    validates :shipping_charge_id, numericality: { other_than: 1 }
+    validates :ship_area_id, numericality: { other_than: 1 }
+    validates :ship_day_id, numericality: { other_than: 1 }
+    validates :category_id, numericality: { other_than: 1 }
   end
 end
