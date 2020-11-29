bafdcab1473bd3f8503c1a7939f8a086b4d32c90
shunxile <69545945+shunxile@users.noreply.github.com>
2020-11-29 15:04:32 +0900

    商品一覧機能　修正①

--- app/javascript/price.js
+++ app/javascript/price.js
@@ -2,7 +2,7 @@ function computation (){
   const price = document.getElementById("item-price");
   const priceCommision = document.getElementById("add-tax-price");
   const profit = document.getElementById("profit");
-  price.addEventListener("keyup", () => {
+  price.addEventListener('keyup', () => {
     const calculationResult = Math.floor(price.value * 0.1);
     priceCommision.innerHTML = calculationResult;
     const salesProfit = price.value - calculationResult;
--- app/models/purchase_history.rb
+++ app/models/purchase_history.rb
@@ -1,5 +1,2 @@
 class PurchaseHistory < ApplicationRecord
-  def index
-    @item = item.all
-  end
 end
