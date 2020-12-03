317c8b2b762e82bec46dab21fd849a98f93fd768
shunxile <69545945+shunxile@users.noreply.github.com>
2020-12-03 13:09:24 +0900

    商品情報編集機能　修正②

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,5 +1,6 @@
 class ItemsController < ApplicationController
   before_action :authenticate_user!, except: [:index, :show]
+  before_action :item_find, only: [:edit, :update]
 
   def index
     @items = Item.includes(:user).order("created_at DESC")
@@ -23,14 +24,15 @@ class ItemsController < ApplicationController
   end
 
   def edit
-    @item = Item.find(params[:id])
     if current_user.id != @item.user.id
       redirect_to root_path
+    end
+    unless user_signed_in?
+      redirect_to user_session_path
     end
   end
 
   def update
-    @item = Item.find(params[:id])
     if @item.update(item_params)
       redirect_to item_path(@item.id)
     else
@@ -41,5 +43,9 @@ class ItemsController < ApplicationController
   private
   def item_params
     params.require(:item).permit(:image, :name, :content, :value, :state_id, :shipping_charge_id, :ship_area_id, :ship_day_id, :category_id).merge(user_id: current_user.id)
+  end
+
+  def item_find
+    @item = Item.find(params[:id])
   end
 end
