348175a77d918c4306259c833538e269f490cc34
shunxile <69545945+shunxile@users.noreply.github.com>
2020-12-16 19:28:42 +0900

    商品編集機能　修正

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -23,7 +23,7 @@ class ItemsController < ApplicationController
   end
 
   def edit
-    if current_user.id != @item.user.id
+    if current_user.id != @item.user.id || @item.purchase_form != nil
       redirect_to root_path
     end
   end
