ab1818c03475033c266aa386ee9544066c85c506
shunxile <69545945+shunxile@users.noreply.github.com>
2020-12-16 14:45:07 +0900

    商品購入機能　修正④

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,6 +1,6 @@
 class ItemsController < ApplicationController
   before_action :authenticate_user!, except: [:index, :show]
-  before_action :item_find, only: [:edit, :update, :destroy]
+  before_action :item_find, only: [:show, :edit, :update, :destroy]
 
   def index
     @items = Item.includes(:user).order("created_at DESC")
@@ -20,7 +20,6 @@ class ItemsController < ApplicationController
   end
 
   def show
-    @item = item_find
   end
 
   def edit
